8db8e83181c172da404a317ee15f57a75578b9b2
Carolyne Njeri <cnjeri388@gmail.com>
2022-08-05 17:27:07 +0300

    TopNavigation and Setuup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import TopNavigation from './Components/TopNavigation';
+
+function App() {
+  return (
+    <div className="App">
+   <h1>Hi Radiant</h1>
+   <TopNavigation/>
+    </div>
+  );
+}
+
+export default App;
